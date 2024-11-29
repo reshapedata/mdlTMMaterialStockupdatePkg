2f4811d140df37d7b511921bce466e72f0a9dccb
reshapedata <29032529@qq.com>
2024-11-29 05:21:49 +0000

    v1.0.1

--- R/materialstock_update.R
+++ R/materialstock_update.R
@@ -36,7 +36,11 @@ insert into rds_erp_src_t_MATERIALSTOCK_bak
 select　'999' AS FUSEORGID,a.fnumber ,
 b.FMINSTOCK ,b.FSAFESTOCK,b.FMAXSTOCK,
 b.FIsEnableMinStock,b.FIsEnableSafeStock ,b.FIsEnableMaxStock,
-c.FMINSTOCK,c.FSAFESTOCK ,c.FMAXSTOCK,GETDATE() AS FUPDATETIME
+c.FMINSTOCK ,c.FSAFESTOCK,c.FMAXSTOCK,
+case c.FIsEnableMinStock when '是' then '1' when '否' then '0'  end as FIsEnableMinStock,
+case c.FIsEnableSafeStock when '是' then '1' when '否' then '0'  end as FIsEnableSafeStock,
+case c.FIsEnableMaxStock when '是' then '1' when '否' then '0'  end as FIsEnableMaxStock,
+GETDATE() AS FUPDATETIME
  from  t_BD_Material　ａ
 inner join t_BD_MaterialStock b　on a.FMATERIALID =b.FMATERIALID
 inner　join　rds_erp_src_t_MATERIALSTOCK_update　c on a.FNUMBER=c.FMATERIALNUMBER
@@ -59,13 +63,13 @@ where  a.FUSEORGID=124854")
 #' materialstock_tm_update()
 materialstock_tm_update <- function(dms_token) {
   sql=paste0("
-UPDATE B
+UPDATE b
 SET 　b.FMINSTOCK =c.FMINSTOCK,
 b.FSAFESTOCK = c.FSAFESTOCK,
 b.FMAXSTOCK=c.FMAXSTOCK,
-b.FIsEnableMinStock=c.FIsEnableMinStock,
-b.FIsEnableSafeStock =c.FIsEnableSafeStock,
-b.FIsEnableMaxStock=c.FIsEnableMaxStock
+b.FIsEnableMinStock=case c.FIsEnableMinStock when '是' then '1' when '否' then '0'  end ,
+b.FIsEnableSafeStock =case c.FIsEnableSafeStock when '是' then '1' when '否' then '0'  end ,
+b.FIsEnableMaxStock=case c.FIsEnableMaxStock when '是' then '1' when '否' then '0'  end
  from  t_BD_Material　ａ
 inner join t_BD_MaterialStock b　on a.FMATERIALID =b.FMATERIALID
 inner　join　rds_erp_src_t_MATERIALSTOCK_update　c on a.FNUMBER=c.FMATERIALNUMBER
